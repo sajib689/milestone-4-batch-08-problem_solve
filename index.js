9d25326e28869180c0070c3f903afe317e79405a
sajib689 <jibonraja11@gmail.com>
2024-02-04 10:40:10 +0600

    km to miles

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-
+// inch to feet converted
 function inchToFeet (inch) {
     const feet = inch / 12
     return feet
 }
+function inchToFeet2 (inch){
+    const feet = parseInt(inch / 12)
+    const inches = inch % 12
+    const result = feet + 'ft' + inches +'in'
+    return result
+}
+// console.log(inchToFeet2(60.87))
 
-console.log(inchToFeet(60.87))
+// mile to kilometers
+
+function mileToKilometers(miles){
+    const km = miles * 1.6
+    return km
+}
+// console.log(mileToKilometers(60.87))
+// km to miles converted
+
+function kmToMiles(km){
+    const miles = km * 0.621371
+    return miles
+}
+console.log(kmToMiles(6))
